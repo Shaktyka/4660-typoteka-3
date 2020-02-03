6f2b527d2910d102fae973ff51649b4b37db2261
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-02-03 13:24:49 +0500

    Добавляет логику в service.js

--- src/constants.js
+++ src/constants.js
@@ -4,13 +4,13 @@ const DEFAULT_COMMAND = `--help`;
 const USER_ARGV_INDEX = 1;
 const OFFERS_AMOUNT_MAX = 1000;
 
-const Message = {
-  OVERHEAD: `Не больше 1000 объявлений`
-};
-
 const ExitCode = {
   SUCCESS: 0,
   ERROR: 1
+};
+
+const Message = {
+  OVERHEAD: `Не больше 1000 объявлений`
 };
 
 module.exports = {
