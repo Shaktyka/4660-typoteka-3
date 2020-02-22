fa068e240a565c9c30bc9bcb3f36529ef5448803
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-02-22 10:46:29 +0500

    Добавляет обработку списка роутов по заданию

--- src/express/index.js
+++ src/express/index.js
@@ -1,9 +1,22 @@
 'use strict';
 
 const express = require(`express`);
+const registerRouter = require(`./routes/register`);
+const loginRouter = require(`./routes/login`);
+const articlesRouter = require(`./routes/articles`);
+const categoriesRouter = require(`./routes/categories`);
+const searchRouter = require(`./routes/search`);
+const myRouter = require(`./routes/my`);
 
 const WORK_PORT = 8080;
 const app = express();
+
+app.use(`/register`, registerRouter);
+app.use(`/login`, loginRouter);
+app.use(`/articles`, articlesRouter);
+app.use(`/categories`, categoriesRouter);
+app.use(`/search`, searchRouter);
+app.use(`/my`, myRouter);
 
 app.get(`/`, (req, res) => res.send(`/`));
 
