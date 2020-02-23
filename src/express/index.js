195bae62b65a455c5f86a253f02cc1b66e975eb7
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-02-23 23:01:39 +0500

    Добавляет использование Pug в файл сервера

--- src/express/index.js
+++ src/express/index.js
@@ -1,15 +1,23 @@
 'use strict';
 
+const path = require(`path`);
 const express = require(`express`);
-const registerRouter = require(`./routes/register`);
 const loginRouter = require(`./routes/login`);
+const registerRouter = require(`./routes/register`);
 const articlesRouter = require(`./routes/articles`);
 const categoriesRouter = require(`./routes/categories`);
 const searchRouter = require(`./routes/search`);
 const myRouter = require(`./routes/my`);
 
 const WORK_PORT = 8080;
+const STATIC_DIR = path.join(__dirname, `../../markup`);
+
 const app = express();
+
+app.set(`views`, path.join(__dirname, `./templates`));
+app.set(`view engine`, `pug`);
+
+app.use(express.static(STATIC_DIR));
 
 app.use(`/register`, registerRouter);
 app.use(`/login`, loginRouter);
@@ -18,7 +26,9 @@ app.use(`/categories`, categoriesRouter);
 app.use(`/search`, searchRouter);
 app.use(`/my`, myRouter);
 
-app.get(`/`, (req, res) => res.send(`/`));
+app.get(`/`, (req, res) => {
+  res.render(`./pages/main`);
+});
 
 app.listen(WORK_PORT, () =>
   console.log(`Сервер запущен на порту: ${WORK_PORT}`));
