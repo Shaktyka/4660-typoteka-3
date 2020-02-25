88e5f64b9689ca02a44f1b680001a6359d4253db
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-02-25 21:24:49 +0500

    Добавляет шаблон страницы categories

--- src/express/routes/categories.js
+++ src/express/routes/categories.js
@@ -4,7 +4,7 @@ const {Router} = require(`express`);
 const categoriesRouter = new Router();
 
 categoriesRouter.get(`/`, (req, res) => {
-  res.send(`../templates/pages/all-categories`);
+  res.render(`../templates/pages/all-categories`);
 });
 
 module.exports = categoriesRouter;
