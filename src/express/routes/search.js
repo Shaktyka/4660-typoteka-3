bede92a4a0dc5290f2021a3eee2a1737ba244208
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-02-25 20:37:26 +0500

    Добавляет шаблон страницы search

--- src/express/routes/search.js
+++ src/express/routes/search.js
@@ -4,7 +4,7 @@ const {Router} = require(`express`);
 const searchRouter = new Router();
 
 searchRouter.get(`/`, (req, res) => {
-  res.render(`../templates/pages/search`)
+  res.render(`../templates/pages/search`);
 });
 
 module.exports = searchRouter;
