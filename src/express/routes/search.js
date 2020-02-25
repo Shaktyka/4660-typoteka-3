1483b14cddc0b89ab6b17be06b0032a3cf36d0bb
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-02-25 18:08:33 +0500

    Добавляет шаблон search

--- src/express/routes/search.js
+++ src/express/routes/search.js
@@ -3,6 +3,8 @@
 const {Router} = require(`express`);
 const searchRouter = new Router();
 
-searchRouter.get(`/`, (req, res) => res.send(`/search`));
+searchRouter.get(`/`, (req, res) => {
+  res.render(`../templates/pages/search`)
+});
 
 module.exports = searchRouter;
