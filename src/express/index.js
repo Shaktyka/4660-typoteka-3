87adccdcaf108d89795b18f3443db10974b980c1
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-03-30 21:25:47 +0600

    Добавляет директорию public для статики

--- src/express/index.js
+++ src/express/index.js
@@ -1,8 +1,7 @@
 'use strict';
 
-const path = require(`path`);
 const express = require(`express`);
-
+const path = require(`path`);
 const homeRouter = require(`./routes/home-routes`);
 const loginRouter = require(`./routes/login-routes`);
 const registerRouter = require(`./routes/register-routes`);
@@ -10,18 +9,17 @@ const articlesRouter = require(`./routes/articles-routes`);
 const categoriesRouter = require(`./routes/categories-routes`);
 const searchRouter = require(`./routes/search-routes`);
 const myRouter = require(`./routes/my-routes`);
-const STATIC_DIR = path.join(__dirname, `../../markup`);
 
 const WORK_PORT = 8080;
 const SERVER_START_MESSAGE = `Сервер запущен на порту:`;
+const PUBLIC_DIR = `public`;
 
 const app = express();
 
-app.set(`views`, path.join(__dirname, `templates`));
+app.set(`views`, path.resolve(__dirname, `templates`));
 app.set(`view engine`, `pug`);
 
-app.use(express.static(STATIC_DIR));
-
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 app.use(`/`, homeRouter);
 app.use(`/register`, registerRouter);
 app.use(`/login`, loginRouter);
