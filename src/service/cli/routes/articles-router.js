64e2a193cec3d20d91e6d2ea5042aa92f16ac2e1
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-04-01 23:09:32 +0600

    Добавляет роутер для выдачи комментариев публикации

--- src/service/cli/routes/articles-router.js
+++ src/service/cli/routes/articles-router.js
@@ -5,6 +5,7 @@ const articlesRouter = new Router();
 const asyncHandler = require(`express-async-handler`);
 const createError = require(`http-errors`);
 const article = require(`../models/article`);
+const chalk = require(`chalk`);
 const {check, validationResult} = require(`express-validator`);
 const {
   HttpCode,
@@ -22,7 +23,7 @@ const {
 articlesRouter.get(`/`, asyncHandler(async (req, res) => {
   try {
     const result = await article.getAll();
-    console.log(ServerMessage.DATA_SENT);
+    console.log(chalk.green(ServerMessage.DATA_SENT));
     res.json(result);
   } catch (err) {
     throw createError(
@@ -38,14 +39,36 @@ articlesRouter.get(`/:articleId`, asyncHandler(async (req, res) => {
     const articleId = req.params.articleId.trim();
     const result = await article.get(articleId);
     if (result) {
-      console.log(ResultMessage.DATA_SENT);
+      console.log(chalk.green(ResultMessage.DATA_SENT));
       return res.json(result);
     } else {
-      console.log(ResultMessage.NOT_FOUND);
+      console.log(chalk.red(ResultMessage.NOT_FOUND));
       return res.status(HttpCode.NOT_FOUND).send(ResultMessage.NOT_FOUND);
     }
   } catch (err) {
-    console.log(err);
+    console.log(chalk.red(err));
+    throw createError(
+        HttpCode.INTERNAL_SERVER_ERROR,
+        {message: SERVER_ERROR_MESSAGE}
+    );
+  }
+}));
+
+// Отдаёт комментарии публикации по id
+articlesRouter.get(`/:articleId/comments`, asyncHandler(async (req, res) => {
+  const articleId = req.params.articleId.trim();
+  if (articleId.length === 0) {
+    throw createError(
+        HttpCode.BAD_REQUEST,
+        {message: NO_ID_MESSAGE}
+    );
+  }
+
+  try {
+    const result = await article.getComments(articleId);
+    console.log(chalk.green(ResultMessage.DATA_SENT));
+    res.json(result);
+  } catch (err) {
     throw createError(
         HttpCode.INTERNAL_SERVER_ERROR,
         {message: SERVER_ERROR_MESSAGE}
