7e64aaad6db9219fc73c37900f5ee79dff06264e
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-04-02 17:50:38 +0600

    Исправляет отдаваемое сервером сообщение

--- src/service/cli/routes/articles-router.js
+++ src/service/cli/routes/articles-router.js
@@ -58,6 +58,7 @@ articlesRouter.get(`/:articleId`, asyncHandler(async (req, res) => {
 // Добавляет новую публикацию
 articlesRouter.post(`/`, asyncHandler(async (req, res) => {
   const articleData = req.body;
+  console.log(articleData);
 
   // const errors = validationResult(req);
   // if (!errors.isEmpty()) {
@@ -159,7 +160,7 @@ articlesRouter.put(`/:articleId/comments`, [
     .trim()
     .escape()
     .isLength({min: CommentRequirements.minLength.VALUE})
-    .withMessage(`CommentRequirements.minLength.ERROR_TEXT CommentRequirements.minLength.VALUE`)
+    .withMessage(`${CommentRequirements.minLength.ERROR_TEXT} ${CommentRequirements.minLength.VALUE}`)
 ], asyncHandler(async (req, res) => {
 
   const errors = validationResult(req);
